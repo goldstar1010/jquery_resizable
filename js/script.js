39c82b48c77b61944f1eca993638f40ddeb46ecc
whiteneji <smartstar1010@outlook.com>
2023-04-10 04:55:59 -0400

    complete project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(".panel-left").resizable({
+  handleSelector: ".splitter",
+  resizeHeight: false
+});
+
+$(".panel-container").resizable({
+  handleSelector: ".splitter-horizontal",
+  resizeWidth: false
+});
